bb3aea43b634ff18c6ca165308c4575792c3b4e0
subhadip541 <subhadip.ssv@gmail.com>
2020-12-30 12:30:12 +0530

    Array

---
+++ Array/src/Initial.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Initial {
+
+	public static void main(String[] args) {
+		Scanner input= new Scanner(System.in);
+		System.out.println("Enter a number : ");
+		int a= input.nextInt();
+		int[] arr=new int[10];
+		
+	}
+
+}
